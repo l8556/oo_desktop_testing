2a82d5d2e2d1aee2ef4074c9b6becc743aad3539
l02 <l8556@outlook.com>
2023-07-07 14:13:31 +0300

    the editor's opening mechanism has been improved

--- frameworks/desktop/desktop_editor.py
+++ frameworks/desktop/desktop_editor.py
@@ -1,23 +1,42 @@
 # -*- coding: utf-8 -*-
-from subprocess import Popen
-from frameworks.host_control import HostInfo
+import os
+from os.path import join, dirname
+from subprocess import Popen, PIPE
+from frameworks.host_control import HostInfo, FileUtils
+
 
 class DesktopEditor:
     def __init__(self, debug_mode: bool = False):
         self.debug_mode = debug_mode
         self.os = HostInfo().os
+        self.tmp_dir = join(os.getcwd(), "tmp")
+        self.log_file = join(self.tmp_dir, "desktop.log")
+        self.create_log_file()
+        self.debug_command = f'--ascdesktop-log-file={"stdout" if HostInfo().os != "windows" else self.log_file}'
 
     def open(self, file_path: str = None):
-        Popen(
+        process = Popen(
             f"{self._generate_running_command()} "
-            f"{'--ascdesktop-support-debug-info' if self.debug_mode else ''} "
+            f"{self.debug_command if self.debug_mode else ''} "
             f"{file_path if file_path else ''}".strip(),
-            shell=True
+            stdout=PIPE, stderr=PIPE, shell=True
         )
+        return process
 
     def close(self):
         # call('killall DesktopEditors', shell=True)
         ...
+
+    def read_log(self, wait_msg):
+        for line in FileUtils.file_reader(self.log_file).split('\n'):
+            print(line) if line else ...
+            if wait_msg == line.strip():
+                self.create_log_file()
+                return
+
+    def create_log_file(self):
+        FileUtils.create_dir(dirname(self.log_file), stdout=False)
+        FileUtils.file_writer(self.log_file, '', mode='w')
 
     def _generate_running_command(self):
         if self.os == 'linux':
