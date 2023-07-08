5ec47ecb1052b7c2ac66a53e6036c2a69c89eb58
l02 <l8556@outlook.com>
2023-07-08 14:59:07 +0300

    test

--- tasks.py
+++ tasks.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import os
+from os.path import join
+
 from invoke import task
 
 from tests.desktop_test import DesktopTest
@@ -9,6 +12,6 @@ def desktop_test(c, version=None, display=False, custom_config=None, telegram=Fa
     DesktopTest(
         version=version,
         display_on=True if not display else False,
-        custom_config=custom_config,
+        custom_config=join(os.getcwd(), 'portal_config.json') if custom_config else None,
         telegram=telegram
     ).run()
